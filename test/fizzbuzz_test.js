aed08efa0ea3cf16e6e5524fc90032d23c2e1dab
dark maul <abustamante@aitbol.com>
2019-02-11 21:37:55 -0400

    test for multiples of three and five

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -6,13 +6,26 @@ var should = require('chai').should; //cuidado con conpatibilidad / expect y sou
 
 var fixzzbuzz = require('../fizzbuzz');
 
-describe('Pruebas para fizzbuzz', function(){
+describe('Pruebas para multiplo de 3', function(){
 
     it('Deberia retornar verdadero dado 6', function(){
         expect(multipleOfThree(6)).equal(true);
     });
 
+    it('Deberia retornar falso dado 5', function(){
+        expect(multipleOfThree(5)).equal(false);
+    });
+    
+})
+
+
+describe('Pruebas para multiplo de 5', function(){
+
     it('Deberia retornar verdadero dado 10 ', function(){
         expect(multipleOfFive(10)).equal(true);
+    });
+
+    it('Deberia retornar falso dado 11 ', function(){
+        expect(multipleOfFive(11)).equal(false);
     });
 })
